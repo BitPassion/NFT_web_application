e69d58aa69e9554a4456681e2310105be4cd95e0
asherhoffman1120 <asher.hoffman1120@gmail.com>
2022-09-17 11:52:34 -0500

    Fix dropdown menu link toprofile page

--- src/Data/dao/userDao.java
+++ src/Data/dao/userDao.java
@@ -5,5 +5,4 @@ import Domain.User;
 public interface userDao extends AbstractDao<User> {
     User findByUsername(User item);
     User updateByUsername(User item);
-    String getSecurityAnswer(String username, int status);
 }
